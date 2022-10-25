bdbf68b99621c2f23a091ecbb14698870239dd60
Reubz <reugeorac@gmail.com>
2022-10-25 14:58:59 +0530

    v2.0.1 - added mongoDB

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
-const express = require('express');
-const path = require('path');
-const apicache = require('apicache');
-const config = require('./config.json')
+const express = require("express");
+const path = require("path");
+const apicache = require("apicache");
+const config = require("./config.json");
+const mongoose = require('mongoose')
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 let cache = apicache.middleware;
 const port = process.env.PORT;
+const mongooseURI = process.env.MONGOOSEURI;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use('/', require(path.join(__dirname, '/routes/shortlinks')))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'comming soon', 'index.html'))
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "comming soon", "index.html"));
 });
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'comming soon', 'index.html'))
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "comming soon", "index.html"));
 });
-app.get('/instagram', (req, res) => {
-    res.redirect(config.socials.instagram)
+app.get("/instagram", (req, res) => {
+    res.redirect(config.socials.instagram);
 });
-app.get('/twitter', (req, res) => {
-    res.redirect(config.socials.twitter)
+app.get("/twitter", (req, res) => {
+    res.redirect(config.socials.twitter);
 });
-app.get('/discord', (req, res) => {
-    res.redirect(config.socials.discord)
+app.get("/discord", (req, res) => {
+    res.redirect(config.socials.discord);
 });
-app.get('/email', (req, res) => {
-    res.redirect(config.socials.email)
+app.get("/email", (req, res) => {
+    res.redirect(config.socials.email);
 });
-app.get('/github', (req, res) => {
-    res.redirect(config.socials.github)
-});
-
-app.get('*', (req, res) => {
-    res.status(404);
+app.get("/github", (req, res) => {
+    res.redirect(config.socials.github);
 });
 
+app.get("*", (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "pages", "error", "404.html"));
+});
 
-app.listen(port, () => console.log(`App Listening at http://localhost:${port}`));
+mongoose.connect(mongooseURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("Connected to DB"))
+    .catch((err) => console.log(err));
+
+app.listen(port, () =>
+    console.log(`App Listening at http://localhost:${port}`)
+);
