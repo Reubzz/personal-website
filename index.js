c8d3cbdcd022d306372dad1799bae885f3d1dd08
Reubz <reugeorac@gmail.com>
2023-05-10 17:11:06 +0530

    Updated projects directory

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use('/urlshortner', require(path.join(__dirname, '/routes/shortlinks')));
-app.use('/projects', require(path.join(__dirname, '/routes/projects')));
+// app.use('/projects', require(path.join(__dirname, '/routes/projects')));
 
 app.get("/", async (req, res) => {
     res.status(200).render("home/home", {
