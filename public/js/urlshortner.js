153db479511fcb50db7177d4d10482b9f712a8c6
Reuben George <reugeorac@gmail.com>
2023-11-06 21:40:19 +0530

    QR Code Added + UI Imporvement

--- public/js/urlshortner.js
+++ public/js/urlshortner.js
@@ -27,3 +27,49 @@ function logoutHoverLeave(username, userId, userRole) {
     const userIcon = '<i class="fa-solid fa-user"></i>';
     userLogoutBtn.innerHTML = `${userIcon} ${username}`
 }
+
+function closepopup(){
+    document.getElementById('popup-wrapper').style.display = 'none';
+    document.body.style.overflow = '';
+}
+
+function popupDisplay(id, slug, href, qr, expireAt, createdAt, username, role, disabled) {
+    document.getElementById('popup-wrapper').style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+
+    document.getElementById('short-url').innerHTML = `https://reubz.io/${slug}`;
+    document.getElementById('long-url').innerHTML = href;
+    document.getElementById('qrcode').src = qr;
+    document.getElementById('expiry-date').innerHTML = 'Upcoming Feature';
+    document.getElementById('created-date').innerHTML = createdAt;
+    document.getElementById('created-by').innerHTML = `${username} (${role})`;
+    document.getElementById('popup-download-btn').href = qr;
+    document.getElementById('popup-go-button').href = href;
+    document.getElementById('copy-text').value = `https://reubz.io/${slug}`;
+
+    let disableBtn = document.getElementById('popup-disable-btn');
+    let deleteBtn = document.getElementById('popup-delete-btn');
+
+    deleteBtn.onclick = function () {deleteLink(id)};
+
+    disableBtn.onclick = function () {disableLink(id)};
+    if(disabled == true) {
+        disableBtn.innerHTML = 'Enable Link <i class="fa-regular fa-eye"></i>';
+    } else {
+        disableBtn.innerHTML = 'Disable Link <i class="fa-regular fa-eye-slash"></i>'
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('popup-wrapper')) {
+        document.getElementById('popup-wrapper').style.display = "none";
+    }
+}
+
+function copyText() {
+    let copyText = document.getElementById("copy-text");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copied the text: " + copyText.value);
+}
