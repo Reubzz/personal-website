133a3a4da2adc5e5184d66df095f2c1e4f715e67
Reuben George <reugeorac@gmail.com>
2023-11-26 17:23:51 +0530

    Added easter eggs to No Button

--- public/js/valentine.js
+++ public/js/valentine.js
@@ -1,20 +1,45 @@
 const button2 = document.getElementById('btn1');
+let noBtnClickCount = 0;
+
 button2.addEventListener('mouseover', function () {
     this.style.left = `${Math.random() * 70}%`;
     this.style.top = `${Math.random() * 70}%`;
-});
-button2.addEventListener('click', function () {
-    this.style.left = `${Math.random() * 80}%`;
-    this.style.top = `${Math.random() * 80}%`;
+
+    noBtnClickCount++ 
+    const keepTryingText = document.getElementById('keep-trying-text');
+
+    if(noBtnClickCount == 5) {
+        keepTryingText.innerHTML = "Hey you're being too mean now! Say Yes!"
+        openModal("dialog-no");
+    }
+    else if (noBtnClickCount == 10) {
+        keepTryingText.innerHTML = "Wow! You need to try harder to Reject me!! UwU"
+        openModal("dialog-no");
+    }
+    else if (noBtnClickCount == 15) {
+        keepTryingText.innerHTML = "Damn! You're really hell bent on saying No!!"
+        openModal("dialog-no");
+    }
+    else if (noBtnClickCount == 20) {
+        keepTryingText.innerHTML = "Okayy! Fine! Bye"
+        openModal("dialog-no");
+    }
 });
 
-const dailog = document.getElementById("dialog");
-function openModal() {
-    dailog.showModal();
+button2.addEventListener('click', function () {
+    // this.style.left = `${Math.random() * 80}%`;
+    // this.style.top = `${Math.random() * 80}%`;
+});
+
+function openModal(id) {
+    document.getElementById(id).showModal();
 }
 
-function closeModal() {
-    dailog.close();
+function closeModal(id) {
+    if(noBtnClickCount == 20) {
+        return window.location.href="https://reubz.io";
+    }
+    document.getElementById(id).close();
 }
 
 
