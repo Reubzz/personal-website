b200bef9a3ec9bdfde7f11e87e3755fb7e76c358
Reuben J George <74487020+Reubzz@users.noreply.github.com>
2024-01-01 00:56:16 +0530

    Update valentine.js

--- public/js/valentine.js
+++ public/js/valentine.js
@@ -26,6 +26,30 @@ button2.addEventListener('mouseover', function () {
     }
 });
 
+button2.addEventListener('click', function () {
+    this.style.left = `${Math.random() * 70}%`;
+    this.style.top = `${Math.random() * 70}%`;
+
+    noBtnClickCount++ 
+    const keepTryingText = document.getElementById('keep-trying-text');
+
+    if(noBtnClickCount == 5) {
+        keepTryingText.innerHTML = "Hey you're being too mean now! Say Yes!"
+        openModal("dialog-no");
+    }
+    else if (noBtnClickCount == 10) {
+        keepTryingText.innerHTML = "Wow! You need to try harder to Reject me!! UwU"
+        openModal("dialog-no");
+    }
+    else if (noBtnClickCount == 15) {
+        keepTryingText.innerHTML = "Damn! You're really hell bent on saying No!!"
+        openModal("dialog-no");
+    }
+    else if (noBtnClickCount == 20) {
+        keepTryingText.innerHTML = "Okayy Fine! Bye!"
+        openModal("dialog-no");
+    }
+});
 
 function openModal(id) {
     document.getElementById(id).showModal();
