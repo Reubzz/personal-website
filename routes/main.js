108ab45b1a472095b1e658480deff7d6c77d3b34
Reuben J George <74487020+Reubzz@users.noreply.github.com>
2024-03-28 13:54:52 -0700

    Updated Resume to the real one

--- routes/main.js
+++ routes/main.js
@@ -68,7 +68,7 @@ router.get("/linkedin", (req, res) => {
     res.redirect(config.socials.linkedin);
 });
 router.get("/resume", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public", "files", "resume-joke.pdf"))
+    res.sendFile(path.join(__dirname, "../public", "files", "resume.pdf"))
 });
 
 /**
